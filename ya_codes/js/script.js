2a1d7bf6a09cbf853544c0ba7fa92b2e300f60aa
hashmaher <130112794+hashmaher@users.noreply.github.com>
2026-01-07 13:36:39 +0900

    Add files via upload
    サイトのアップロード

---
+++ ya_codes/js/script.js
@@ -0,0 +1,34 @@
+$(function() {
+    
+    //ジャンプのスクロール機能-----------------------------------------------   
+    $('.jump').on('click', function(e) {
+        e.preventDefault();
+        
+        const target = $(this).attr('href');
+        let pos = $(target).offset().top - 170; //const禁止
+
+        if (target === '#footer') {
+            pos += 120;
+        } else if (target === '#top') {
+            pos = 0;
+        }
+
+
+        $('html, body').animate({scrollTop: pos}, 300);
+
+    });
+
+    //トップへ戻るボタン-----------------------------------------------
+    $(window).on('scroll', function() {
+        const scroll = $(this).scrollTop();
+
+        if (scroll > 300) {
+            $('.to_top').fadeIn();
+        } else {
+            $('.to_top').fadeOut();
+        }
+    });
+
+
+  
+});
